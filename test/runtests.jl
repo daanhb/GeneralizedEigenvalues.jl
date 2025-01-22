d274b42e7e78e1604ef16ade583419e5b57ae596
Daan Huybrechs <daan.huybrechs@cs.kuleuven.be>
2025-01-22 11:48:23 +0100

    tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,11 @@
-using GeneralizedEigenvalueProblems
+using GeneralizedEigenvalues
+using LinearAlgebra
 using Test
 
-@testset "GeneralizedEigenvalueProblems.jl" begin
-    # Write your tests here.
+@testset "GeneralizedEigenvalues.jl" begin
+    A = rand(BigFloat, 4, 4)
+    B = rand(BigFloat, 4, 4)
+    vals = eigvals(Float64.(A), Float64.(B))
+    genvals = eigvals(A, B)
+    @test norm(vals-genvals) < 1e-10
 end
